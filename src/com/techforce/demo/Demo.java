b7ba87378d3c134a1f576746d3f797ba6c6c136c
mehulgajera77 <mehul.gajera77@gmail.com>
2020-08-20 16:15:34 +0530

    Add One Show() method in Demo class

--- src/com/techforce/demo/Demo.java
+++ src/com/techforce/demo/Demo.java
@@ -2,9 +2,19 @@ package com.techforce.demo;
 
 public class Demo {
 	
+	/*
+	 * this method will show or print the output on Console
+	 */
+	void show()
+	{
+		System.out.println("Hello World Some Changes");
+	}
+	
 	public static void main(String[] args)
 	{
 		System.out.println("Hello World For Git Demo");
+		Demo demo=new Demo();
+		demo.show();
 	}
 
 }
